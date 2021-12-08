3d3bc3a540096a461cfc3cd965b9d45f6e093b75
Shaun Emery <shaunsheep18@gmail.com>
2021-12-08 11:41:15 +0000

    added name to Pet constructor

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -4,4 +4,7 @@ describe('constructor', () => {
     it('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
+    it('sets pet name to Fido', () => {
+      expect(new Pet('Fido')).toEqual({name: 'Fido'});
+    });
   });
