736f47169fb7bca84f810b15dd962a47bbb46268
Shaun Emery <shaunsheep18@gmail.com>
2021-12-08 13:49:41 +0000

    added fitness and hunger

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -12,6 +12,14 @@ describe('constructor', () => {
         const pet = new Pet('Fido');
         expect(pet.age).toEqual(0);
     });
+    it('sets inital hunger to 0', () => {
+        const pet = new Pet('Fido');
+        expect(pet.hunger).toEqual(0);
+    });
+    it('sets initial fitness to 10', () => {
+        const pet = new Pet('Fido');
+        expect (pet.fitness).toEqual(10);
+    });       
 });
 
 describe('growUp', () => {
@@ -19,5 +27,15 @@ describe('growUp', () => {
         const pet = new Pet('Fido');
         pet.growUp();
         expect(pet.age).toEqual(1);
+    });
+    it('increases the pet hunger by 5', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.hunger).toEqual(5);
+    });
+    it('decreases the pet fitness by 3', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+        expect(pet.fitness).toEqual(7);
     });
 });
